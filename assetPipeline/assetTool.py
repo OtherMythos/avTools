9e8673c255979423c896018567754a39c2b571d7
edherbert <edward@othermythos.com>
2020-08-01 20:42:25 +0100

    Added an option to purge xml files from the blender export.

--- assetPipeline/assetTool.py
+++ assetPipeline/assetTool.py
@@ -3,6 +3,7 @@
 import argparse
 from directoryScanner import DirectoryScanner
 from exportManager import ExportManager
+from pathlib import Path
 
 def main():
     helpText = '''A script capable of taking asset project files and producting resources which can be accepted by the avEngine.
@@ -18,6 +19,8 @@ def main():
     parser.add_argument('InputDirectory', metavar='I', type=str, nargs='?', help='A path to the input directory.', default=None)
     parser.add_argument('OutputDirectory', metavar='O', type=str, nargs='?', help='A path to the output directory.', default=None)
 
+    parser.add_argument("-p", "--purge", help="Purge XML files produced from blender export", default=True, type=bool, nargs='?')
+
     parser.add_argument("-b", "--blender", help="The path to a blender executable.", default="blender")
     args = parser.parse_args()
 
@@ -25,6 +28,10 @@ def main():
         print("Please provide both an input and output directory path")
         return
 
+
+    shouldPurgeXML = False
+    if args.purge is None:
+        shouldPurgeXML = True
 
     exporter = ExportManager(args.blender)
     if not exporter.executableValid():
@@ -36,6 +43,10 @@ def main():
     if not result:
         #Something failed
         return
+
+    outputPath = Path(args.OutputDirectory)
+    if shouldPurgeXML:
+        exporter.recursivePurgeXMLMeshes(outputPath)
 
 if __name__ == "__main__":
     main()
--- assetPipeline/exportManager.py
+++ assetPipeline/exportManager.py
@@ -1,11 +1,20 @@
 import subprocess
 import os
-
+from pathlib import Path
 
 class ExportManager:
 
     def __init__(self, blenderPath):
         self.blenderPath = blenderPath
+
+    def recursivePurgeXMLMeshes(self, targetDirectory):
+        print("Purging .mesh.xml files in directory: %s" % str(targetDirectory) )
+        for root, subdirs, files in os.walk( str(targetDirectory) ):
+            for i in files:
+                if ".mesh.xml" in i:
+                    #Delete the file
+                    targetFile = Path(root) / i
+                    os.remove(targetFile)
 
     def executableValid(self):
         if not os.path.isfile(self.blenderPath):
@@ -22,4 +31,5 @@ class ExportManager:
         #./blender -b ~/Documents/meshTests/newMesh/Grave.blend --python ~/Documents/avTools/exportTest.py -- /tmp/3
         devnull = open(os.devnull, 'w')
         process = subprocess.Popen([str(self.blenderPath), "-b", str(filePath), "--python", "../blenderExporter/exportTest.py", "--", str(outputPath)], stdout=devnull, stderr=devnull)
+        process.wait()
         devnull.close()
