565ba864a8eb515737038796b873aa54c6c0560a
edherbert <edward@othermythos.com>
2020-10-18 12:57:31 +0100

    Ported the mesh viewer to Windows.

--- meshViewer/meshViewer.py
+++ meshViewer/meshViewer.py
@@ -6,20 +6,38 @@ import os
 import subprocess
 from pathlib import Path
 import shutil
+import tempfile
+
+def getTmpPath():
+    return tempfile.gettempdir()
 
 def getSetupFileOutput():
-    #On windows this would be something different, which is why I made it a function.
-    return "/tmp/avSetup.cfg"
+    path = getTmpPath()
+    return str(Path(path) / "avSetup.cfg")
+
+def getOgresResourcesFileOutput():
+    path = getTmpPath()
+    return str(Path(path) / "OgreResources.cfg")
+
+def getExpectedToolsPath():
+    return str(Path.home() / "Documents/avTools/")
+
+def getExpectedEnginePath():
+    enginePath = Path.home() / "Documents/avEngine/"
+    if os.name == "nt": #Windows
+        enginePath = enginePath / "build/Debug/av.exe"
+    else:
+        enginePath = enginePath / "build/av"
+
+    return str(enginePath)
 
 '''
 Create a directory somewhere temporary which will behave as the current working directory.
 This is to make sure the hlms shaders or other ogre stuff ends up somewhere other than one of the user's directories.
 '''
 def getTemporaryCWD():
-    #On windows this will be something else.
-    targetDir = "/tmp"
 
-    target = Path(targetDir) / "meshViewer"
+    target = Path(getTmpPath()) / "meshViewer"
     if target.exists():
         shutil.rmtree(target)
     target.mkdir()
@@ -33,10 +51,10 @@ def produceSetupFile(meshPath):
     objectPath = Path(meshPath)
     if(not objectPath.exists()):
         print("Provided mesh path does not exist.")
-        return False
+        return ""
     if(not objectPath.is_file()):
         print("Please provide a path to a file.")
-        return False
+        return ""
 
     objectPath = objectPath.resolve()
     assert objectPath.exists()
@@ -44,12 +62,20 @@ def produceSetupFile(meshPath):
     resourceLocations.append(objectPath.parent)
     targetMesh = objectPath.name
 
+    ogreResourcesFile = getOgresResourcesFileOutput()
+    expectedToolsPath = getExpectedToolsPath()
+    finalToolsPath = Path(expectedToolsPath) / "meshViewer/squirrel/"
+    if not finalToolsPath.exists() or not finalToolsPath.is_dir():
+        print("No valid path to the avTools directory. Checked %s" % str(finalToolsPath))
+        return ""
+    print("Squirrel files found at path: %s" % finalToolsPath)
+
     data = {}
     data["Project"] = "Mesh Viewer"
     data["CompositorBackground"] = "0.156 0.184 0.215 1"
     data["SquirrelEntryFile"] = "main.nut"
-    data["DataDirectory"] = "/home/edward/Documents/avTools/meshViewer/squirrel/"
-    data["OgreResourcesFile"] = "/tmp/OgreResources.cfg"
+    data["DataDirectory"] = str(finalToolsPath)
+    data["OgreResourcesFile"] = ogreResourcesFile
     data["NumWorkerThreads"] = 1 #The minimum number
     data["UserSettings"] = {
         "targetMesh" : targetMesh
@@ -58,16 +84,17 @@ def produceSetupFile(meshPath):
         "disabled" : True
     }
 
-    with open(getSetupFileOutput(), 'w') as outfile:
+    outFilePath = getSetupFileOutput()
+    print("Writing avSetup.cfg file to %s" % outFilePath)
+    with open(outFilePath, 'w') as outfile:
         json.dump(data, outfile)
 
-    #TODO temporary. In future I'm going to use the avSetup file resource locations.
-    with open("/tmp/OgreResources.cfg", 'w') as outfile:
+    with open(ogreResourcesFile, 'w') as outfile:
         outfile.write('[General]\n')
         for i in resourceLocations:
             outfile.write('FileSystem=' + str(i))
 
-    return True
+    return outFilePath
 
 def main():
     helpText = '''A tool to inspect ogre meshes in the avEngine.
@@ -86,14 +113,17 @@ def main():
         print("Please provide a path to a mesh or avSetup.cfg file")
         return
 
-    result = produceSetupFile(targetPath)
-    if(not result):
+    targetResultPath = produceSetupFile(targetPath)
+    if(not targetResultPath):
         return
 
 
     targetCWD = getTemporaryCWD()
     devnull = open(os.devnull, 'w')
-    process = subprocess.Popen(["/home/edward/Documents/avEngine/build/av", "/tmp/avSetup.cfg"], stdout=devnull, stderr=devnull, cwd=str(targetCWD))
+
+    enginePath = getExpectedEnginePath()
+
+    process = subprocess.Popen([enginePath, targetResultPath], stdout=devnull, stderr=devnull, cwd=str(targetCWD))
     devnull.close()
 
 
