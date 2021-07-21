899a4ff4507d90362fff7d3823a7e55859619c32
edherbert <edward@othermythos.com>
2021-07-21 17:23:11 +0100

    Added depenencies for building xcf files to the container.

--- assetPipelineContainer/entrypoint.sh
+++ assetPipelineContainer/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -xe
 
+#Start this up so gimp can be used in the container.
+Xvfb :99 -screen 0 640x480x8 -nolisten tcp &
+
 cd /scripts/assetPipeline
 
 ./assetTool.py -b /usr/bin/blender /buildInput/ /buildOutput/
