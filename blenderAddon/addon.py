1566b188f7521def2e76f7791a89464bc1ca6ddb
edherbert <edward@othermythos.com>
2022-03-05 18:59:48 +0000

    Improvements. Started adding a view in project option.

--- blenderAddon/addon.py
+++ blenderAddon/addon.py
@@ -1,5 +1,5 @@
 bl_info = {
-    "name": "Random addon for me",
+    "name": "avEngine helper",
     "blender": (2, 80, 0),
     "category": "Object",
 }
@@ -15,34 +15,53 @@ from io_ogre.ogre.mesh import dot_mesh
 from io_ogre.ogre.skeleton import dot_skeleton
 
 
+_CONFIG_TAGS_ = ['AV_ENGINE_PATH', 'MATERIAL_EDITOR_AV_SETUP', 'PROJECT_AV_SETUP']
 av_plugin_config = {
-    "engine_path": "/Users/edward/Documents/avEngine/build/Debug/av.app/Contents/MacOS/av",
-    "material_editor_av_setup": "/Users/edward/Documents/materialEditor/avSetup.cfg",
+    _CONFIG_TAGS_[0]: "/Users/edward/Documents/avEngine/build/Debug/av.app/Contents/MacOS/av",
+    _CONFIG_TAGS_[1]: "/Users/edward/Documents/materialEditor/avSetup.cfg",
+    _CONFIG_TAGS_[2]: "/Users/edward/Documents/rpgGame/avSetup.cfg",
 }
-_CONFIG_TAGS_ = ['AV_ENGINE_PATH', 'MATERIAL_EDITOR_AV_SETUP']
+
+def isEngineSettingsValid():
+    global av_plugin_config
+
+    targetPath = Path(av_plugin_config[_CONFIG_TAGS_[0]])
+    if not targetPath.exists() or not targetPath.is_file():
+        return False
+    targetPath = Path(av_plugin_config[_CONFIG_TAGS_[1]])
+    if not targetPath.exists() or not targetPath.is_file():
+        return False
+
+    return True
 
 class avEngineBlenderAddonPreferences(bpy.types.AddonPreferences):
     bl_idname = __name__
 
     def apply_preferences_to_config(self, context):
+        global av_plugin_config
         addon_preferences = context.preferences.addons[__name__].preferences
 
         for key in _CONFIG_TAGS_:
             addon_pref_value = getattr(addon_preferences,key,None)
             if addon_pref_value is not None:
                 av_plugin_config[key] = addon_pref_value
-                print(addon_pref_value)
 
     AV_ENGINE_PATH: bpy.props.StringProperty(
         name=_CONFIG_TAGS_[0],
         subtype='FILE_PATH',
-        default=av_plugin_config["engine_path"],
+        default=av_plugin_config[_CONFIG_TAGS_[0]],
         update=apply_preferences_to_config
     )
     MATERIAL_EDITOR_AV_SETUP: bpy.props.StringProperty(
         name=_CONFIG_TAGS_[1],
         subtype='FILE_PATH',
-        default=av_plugin_config["material_editor_av_setup"],
+        default=av_plugin_config[_CONFIG_TAGS_[1]],
+        update=apply_preferences_to_config
+    )
+    PROJECT_AV_SETUP: bpy.props.StringProperty(
+        name=_CONFIG_TAGS_[2],
+        subtype='FILE_PATH',
+        default=av_plugin_config[_CONFIG_TAGS_[2]],
         update=apply_preferences_to_config
     )
 
@@ -50,8 +69,10 @@ class avEngineBlenderAddonPreferences(bpy.types.AddonPreferences):
         layout = self.layout
         layout.prop(self, _CONFIG_TAGS_[0])
         layout.prop(self, _CONFIG_TAGS_[1])
+        layout.prop(self, _CONFIG_TAGS_[2])
 
 class avEngineExportBase(bpy.types.Operator):
+
     def getTemporaryDir(self):
         #For unix alone.
         start = "/tmp"
@@ -153,18 +174,62 @@ class avEngineExportBase(bpy.types.Operator):
 
         createdSetupFile = self.createAvSetupFile(tempDir, firstMesh, str(tempDir / self.getProjectMaterialFile()))
 
-        devnull = open(os.devnull, 'w')
-        pathToEngineExecutable = av_plugin_config["engine_path"]
-        pathToMaterialEditor = av_plugin_config["material_editor_av_setup"]
-        process = subprocess.Popen([pathToEngineExecutable, pathToMaterialEditor, createdSetupFile], stdout=devnull, stderr=devnull)
-        devnull.close()
+        pathToEngineExecutable = av_plugin_config[_CONFIG_TAGS_[0]]
+        pathToMaterialEditor = av_plugin_config[_CONFIG_TAGS_[1]]
+        self._openEngineWithArgs([pathToEngineExecutable, pathToMaterialEditor, createdSetupFile])
 
+    def _openEngineWithArgs(self, args):
+        devnull = open(os.devnull, 'w')
+        process = subprocess.Popen(args, stdout=devnull, stderr=devnull)
+        devnull.close()
 
 class avEngineViewInEngine(avEngineExportBase):
     """View in avEngine"""
     bl_idname = "avengine.view_in_engine"
     bl_label = "View in engine"
     bl_options = {'REGISTER'}
+
+    @classmethod
+    def poll(cls, context):
+        return isEngineSettingsValid()
+
+    def execute(self, context):
+        scene = context.scene
+        self.viewingInEngine()
+
+        return {'FINISHED'}
+
+class avEngineViewInProject(avEngineExportBase):
+    """View in project, loading the various resources of the project."""
+    bl_idname = "avengine.view_in_project"
+    bl_label = "View in project"
+    bl_options = {'REGISTER'}
+
+    @classmethod
+    def poll(cls, context):
+        firstValid = isEngineSettingsValid()
+
+        #Check the project file exists.
+        targetPath = Path(av_plugin_config[_CONFIG_TAGS_[2]])
+        secondValue = False
+        if targetPath.exists() and targetPath.is_file():
+            secondValue = True
+
+        return firstValid and secondValue
+
+    def viewingInEngine(self):
+        print("Viewing single object in engine with project.")
+
+        #Generate avSetup file.
+        tempDir = self.getTemporaryDir()
+        firstMesh = self.exportMeshes(tempDir)
+
+        createdSetupFile = self.createAvSetupFile(tempDir, firstMesh, str(tempDir / self.getProjectMaterialFile()))
+
+        pathToEngineExecutable = av_plugin_config[_CONFIG_TAGS_[0]]
+        pathToMaterialEditor = av_plugin_config[_CONFIG_TAGS_[1]]
+        projectSetupFile = av_plugin_config[_CONFIG_TAGS_[2]]
+        self._openEngineWithArgs([pathToEngineExecutable, projectSetupFile, pathToMaterialEditor, createdSetupFile])
 
     def execute(self, context):
         scene = context.scene
@@ -177,6 +242,7 @@ class VIEW3D_MT_menu(bpy.types.Menu):
 
     def draw(self, context):
         self.layout.operator(avEngineViewInEngine.bl_idname)
+        self.layout.operator(avEngineViewInProject.bl_idname)
 
 def addmenu_callback(self, context):
     self.layout.menu("VIEW3D_MT_menu")
@@ -187,6 +253,7 @@ def add_preview_button(self, context):
 
 def register():
     bpy.utils.register_class(avEngineViewInEngine)
+    bpy.utils.register_class(avEngineViewInProject)
     bpy.utils.register_class(VIEW3D_MT_menu)
     bpy.utils.register_class(avEngineBlenderAddonPreferences)
 
@@ -197,6 +264,7 @@ def register():
 def unregister():
     bpy.utils.unregister_class(avEngineViewInEngine)
     bpy.utils.unregister_class(avEngineBlenderAddonPreferences)
+    bpy.utils.unregister_class(avEngineViewInProject)
 
     bpy.types.VIEW3D_HT_header.remove(addmenu_callback)
     bpy.utils.unregister_class(VIEW3D_MT_menu)
